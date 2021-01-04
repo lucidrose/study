f86aa13f9c1a41642fa5e0833c38455b3b036953
lucidrose621 <june_621@naver.com>
2021-01-05 01:18:22 +0900

    2021.01.04 react css styling

--- react/styling-react/src/App.js
+++ react/styling-react/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component} from 'react';
 import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponents'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <div>
+          <SassComponent />
+        </div>
+        <div>
+          <CSSModule />
+        </div>
+        <div>
+          <StyledComponent />
+        </div>
       </div>
     )
   }
