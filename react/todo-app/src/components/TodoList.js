4fbbacab329350528355366e02d1b3ec391be2f1
lucidrose621 <june_621@naver.com>
2021-01-08 02:56:44 +0900

    2021.01.07 react.memo, react.Reducer

--- react/todo-app/src/components/TodoList.js
+++ react/todo-app/src/components/TodoList.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import {useCallback} from 'react'
+import {List} from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
-    return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem 
-                    todo={todo} 
-                    key={todo.id} 
+    const rowRenderer = useCallback (
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem
+                    todo={todo}
+                    key={key}
                     onRemove={onRemove}
-                    onToggle={onToggle}/>
-            ))}
-        </div>
+                    onToggle={onToggle}
+                    style={style}
+                    />
+            )
+        },
+        [onRemove, onToggle, todos]
+    )
+    return (
+        <List
+            className="TodoList"
+            width={496}
+            heigth={513}
+            rowCount={todos.length}
+            rowHeight={56}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline:'none'}}
+            />
     )
 }
-export default TodoList;
+export default React.memo(TodoList);
